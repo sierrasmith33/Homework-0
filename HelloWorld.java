483dd823e358fd0ad3b1b993bb4212f339bbe663
SIERRA SMITH <ssmith2@albany.edu>
2016-09-04 08:51:43 -0400

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//Sierra Smith
+//Homework 0
+//ICSI 213
+//September 4, 2016
+//This program will print Hello World
+
+public class HelloWorld {
+  
+  public static void main (String [] args){
+  
+  
+  }
+}
